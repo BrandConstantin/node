0c33e9766e0e467d16865ddc915d095bdb38afd9
BrandConstantin <brand_constantin@hotmail.com>
2017-01-31 13:45:28 +0100

    aprendiendo express y jade

--- ejercicios/formulario/server.js
+++ ejercicios/formulario/server.js
@@ -3,36 +3,36 @@ var url = require("url");
 //parsear la linea de los parámetros
 var querystring = require('querystring');
 
-function iniciar(route, handler){
-  function onRequest(request, response) {
-    var rutaUrl = url.parse(request.url).pathname;
+function iniciar(route, handler) {
+    function onRequest(request, response) {
+        var rutaUrl = url.parse(request.url).pathname;
 
-    //parte la url a partir de ?
-    var resultado = url.parse(request.url).split('?')[1];
-    //se puede hacer de este modo también
-    //var urlEnArray = request.url.split('?');
-    //var resultado = urlEnArray[1];
-    console.log(resultado);
-    var a = parseInt(querystring.parse(resultado)['a']);
-    var b = parseInt(querystring.parse(resultado)['b']);
-    var c = parseInt(querystring.parse(resultado)['c']);
-    
-    var numeros = [a, b, c];
-    console.log(numeros);
+        //parte la url a partir de ?
+        var resultado = url.parse(request.url).split('?')[1];
+        //se puede hacer de este modo también
+        //var urlEnArray = request.url.split('?');
+        //var resultado = urlEnArray[1];
+        //console.log(resultado);
+        var a = parseInt(querystring.parse(resultado)['a']);
+        var b = parseInt(querystring.parse(resultado)['b']);
+        var c = parseInt(querystring.parse(resultado)['c']);
 
-    console.log("Petición para " + rutaUrl + " recibida.");
-    console.log("Peticion Recibida.");
+        var numeros = [a, b, c];
+        console.log(numeros);
 
-    var contenido = route(rutaUrl, handler, resultado);
-    
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(contenido);
-    response.end();
-  }
+        console.log("Petición para " + rutaUrl + " recibida.");
+        console.log("Peticion Recibida.");
 
-  http.createServer(onRequest).listen(8888);
+        var contenido = route(rutaUrl, handler, resultado);
 
-  console.log("Servidor Iniciado en http://localhost:8888");
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(contenido);
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+
+    console.log("Servidor Iniciado en http://localhost:8888");
 }
 
 exports.iniciar = iniciar;
