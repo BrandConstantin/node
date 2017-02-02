f99187e2f09184feae2cfe2afe56ef2fb062d4e1
BrandConstantin <brand_constantin@hotmail.com>
2017-02-02 12:07:05 +0100

    express + jade

---
+++ express/express-jade/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+var direccion = {
+    direccion: 'Marie Cueri, 10 - PTA, Campanillas, Málaga'
+}
+
+app.get('/contacto', function(req, res) {
+    res.render('contacto', direccion);
+});
+
+var datos = {
+    nombre: 'Aitor',
+    apellido: 'Jahson',
+}
+
+app.get('/usuarios', function(req, res) {
+    res.render('usuarios', datos);
+});
+
+app.get('/saluda/:nombre', function(req, res) {
+    var persona = {
+        'nombre': req.params.nombre
+    };
+
+    res.render('saluda', persona);
+});
+
+app.get('/formulario', function(req, res) {
+    res.render('formulario');
+});
+
+app.listen(8080);
